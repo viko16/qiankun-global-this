f106e87cc7722db5cd595181d6997b7213a925a1
viko16 <16viko@gmail.com>
2023-02-15 00:05:30 +0800

    add demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
-import yayJpg from '../assets/yay.jpg';
+import { loadMicroApp } from 'qiankun';
+import { useEffect, useRef } from 'react';
 
 export default function HomePage() {
+  const container = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    loadMicroApp({
+      name: 'demo',
+      entry: 'http://localhost:2333',
+      container: container.current || '#container',
+    });
+  }, []);
+
   return (
     <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
+      <h2>这是父应用</h2>
+      <p>{navigator.userAgent}</p>
+
+      {/* 这里渲染子应用 */}
+      <div id="container" ref={container} style={{ border: '1px solid red' }} />
     </div>
   );
 }
